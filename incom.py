c8ccd9274ba79b0ddf95f65791b323133baadc35
johnny-sky91 <johnny-sky91@users.noreply.github.com>
2021-12-10 15:03:58 +0100

    Update incom.py

--- incom.py
+++ incom.py
@@ -2,8 +2,6 @@ from app import app, db
 from app.models import User, InCom
 
 
-# u:Spawalnia, p:dupa, e: spawalnia@example.com
-
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'RW': InCom}
