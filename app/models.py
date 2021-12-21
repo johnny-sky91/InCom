acdc53c8b6e316cdf57527aa2391155774730deb
johnny-sky91 <johnny-sky91@users.noreply.github.com>
2021-12-21 09:26:34 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,8 +1,7 @@
+from app import db, login
 from datetime import datetime
-from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from app import login
 
 
 class User(UserMixin, db.Model):
