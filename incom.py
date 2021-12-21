7d924728c80db5aea4b1e7099dd7683354eb4e73
johnny-sky91 <johnny-sky91@users.noreply.github.com>
2021-12-21 10:25:47 +0100

    Update incom.py

--- incom.py
+++ incom.py
@@ -1,7 +1,8 @@
 from app import app, db
-from app.models import User, InCom
+from app.models import User, InCom, Models, Types, Causes, DetectionAreas
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'RW': InCom}
+    return {'db': db, 'User': User, 'InCom': InCom,
+            'Types': Types, 'Models': Models, 'Causes': Causes, 'DetectionAreas': DetectionAreas}
