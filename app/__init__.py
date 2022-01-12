5c7d4b6754df44060194bca4f8339eeb3ace9a6a
johnny-sky91 <johnny-sky91@users.noreply.github.com>
2022-01-12 13:53:28 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -17,7 +17,7 @@ login.login_view = 'login'
 if not app.debug:
     if not os.path.exists('logs'):
         os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/incom.log', maxBytes=10240,backupCount=10)
+    file_handler = RotatingFileHandler('logs/incom.log', maxBytes=10240, backupCount=10)
     file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
