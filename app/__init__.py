fed5e30af379e8d056773c300dfeb81630da1575
johnny-sky91 <johnny-sky91@users.noreply.github.com>
2022-02-09 14:00:34 +0100

    Removed flask-moment

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,6 @@ from flask import request
 from flask_babel import Babel
 from flask_login import LoginManager
 from flask_migrate import Migrate
-from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
@@ -18,7 +17,6 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
-moment = Moment(app)
 babel = Babel(app)
 
 if not app.debug:
